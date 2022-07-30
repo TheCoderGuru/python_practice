3485499e30b39d9d2fdf328cc230ee7b6afcc16b
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-07-30 19:08:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Exercise 4: Remove first n characters from a string
+
+str = input('Please enter a string: ');
+char = input('Please enter the number of characters that you would like to be removed: ');
+
+character = int(char)
+print(str[character:])
