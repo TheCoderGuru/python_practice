434f388b6bfb42f20c630f86b495895e00db66af
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-01 18:48:18 -0400

    Create returnCountOfSubstring.py

---
+++ returnCountOfSubstring.py
@@ -0,0 +1,9 @@
+# Exercise 7: Return the count of a given substring from a string
+
+str_x = "Emma is good developer. Emma is a writer"
+
+# Retrieve the word emma from the string
+
+print( str_x.count('Emma') )
+
+print("Emma appeared %s times" % str_x.count('Emma') )
