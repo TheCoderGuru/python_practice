ebcfe6f1b1efc0cf97d7b515da932ce14dc08347
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-01 18:47:54 -0400

    Create printSum.py

---
+++ printSum.py
@@ -0,0 +1,15 @@
+print( "Printing current and previous number and their sum in a range(10)" )
+
+previous_num = 0
+
+# loop from 1 to 10
+
+for i in range( 1, 10 ):
+  
+    print("Current Number", i, "Previous Number ", previous_num, " Sum: ", previous_num + i)
+    
+    # modify previous number
+    
+    # set it to the current number
+    
+    previous_num = i
