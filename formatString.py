fe276b280922a6475997fd83cf9b5a87da97d25b
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:45:53 -0400

    Create formatString.py

---
+++ formatString.py
@@ -0,0 +1,9 @@
+# Exercise 8: Format variables using a string.format() method.
+
+totalMoney = 1000
+quantity = 3
+price = 450
+
+result = "I have {} dollars so I can buy {} football for {} dollars.".format(totalMoney, quantity, price)
+
+print(result)
