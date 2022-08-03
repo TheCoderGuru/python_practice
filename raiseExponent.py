abfc056236ea4ca05a05643da23a2481261d3955
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:53:51 -0400

    Create raiseExponent.py

---
+++ raiseExponent.py
@@ -0,0 +1,12 @@
+# Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+base = 2
+exponent = 5
+
+def power( a, b ):
+  return a ** b;
+
+
+print(power(base, exponent))
+
+print('2 raises to the power of 5: 32 i.e. (2 *2 * 2 *2 *2 = 32)')
