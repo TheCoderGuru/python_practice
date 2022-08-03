8d59007af26fb6e3ffc3f42841bc95b3fbd277a6
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:53:23 -0400

    Create printPattern.py

---
+++ printPattern.py
@@ -0,0 +1,11 @@
+# Exercise 8: Print the following pattern
+
+for num in range(10):
+  
+    for i in range(num):
+      
+        print (num, end=" ") #print number
+      
+    # new line after each row to display pattern correctly
+      
+    print("\n")
