bddbe28a5eda5066175ed929307926924e265486
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:46:45 -0400

    Create printHalfPyramid.py

---
+++ printHalfPyramid.py
@@ -0,0 +1,9 @@
+# Exercise 14: Print downward Half-Pyramid Pattern with Star (asterisk)
+
+for x in range(6, 0, -1):
+  
+  for j in range(0, x - 1):
+    
+    print("*", end=" ")
+    
+  print(" ")
