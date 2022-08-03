42bb034cd786aebd952460e2865eaa908a39981c
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:44:02 -0400

    Create displayThreeString.py

---
+++ displayThreeString.py
@@ -0,0 +1,9 @@
+# Exercise 2: Display three string “Name”, “Is”, “James” as “Name**Is**James”
+
+first_string = input( 'Please enter the first string: ' );
+
+second_string = input( 'Please enter the second string: ' );
+
+third_string = input( 'Please enter the third string: ' );
+
+print( '"%s**%s**%s"' % ( first_string, second_string, third_string ) )
