e2145bbe6ece5737bee1d815312cefb8525415c4
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:54:39 -0400

    Create reverseExtractedDigit.py

---
+++ reverseExtractedDigit.py
@@ -0,0 +1,10 @@
+# Exercise 11: Write a Program to extract each digit from an integer in the reverse order.
+
+num = 10
+
+number = str(num)
+
+print( number[::-1] )
+
+for x in number[::-1]:
+  print( x )
