f08c78f4d9e7897599cb8d5a2275ce6453dce24d
Shashree Samuel <86580705+TheCoderGuru@users.noreply.github.com>
2022-08-02 21:44:30 -0400

    Create fiveNumberFloatList.py

---
+++ fiveNumberFloatList.py
@@ -0,0 +1,11 @@
+# Exercise 5: Accept a list of 5 float numbers as an input from the user
+
+
+numbers = input( 'Please enter five float numbers: ' )
+
+numbers_split = numbers.split()
+
+list = []
+
+list.append(numbers_split)
+print(list)
